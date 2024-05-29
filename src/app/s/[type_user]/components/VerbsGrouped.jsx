125ea1ebfcd93127594451ea43f463d375172feb
RubenAcosta16 <1511ruben@gmail.com>
2024-05-28 20:16:08 -0600

    aa

--- src/app/s/[type_user]/components/VerbsGrouped.jsx
+++ src/app/s/[type_user]/components/VerbsGrouped.jsx
@@ -6,13 +6,13 @@ const VerbsGrouped = ({ verbGrouped, functionNav,hasImg }) => {
   // console.log(verbGrouped)
   // const i=1
   return (
-    <li>
+    <li className="mb-[15px]">
       {functionNav == "Normal" || functionNav == "Significados" ? (
         <>
           <p className="text-[20px] text-normal font-bold">
             Grupo: <span className="font-normal">{verbGrouped[0].group ? verbGrouped[0].group : "Sin grupo"}</span>
           </p>
-          <ul className="flex flex-col gap-[15px] border-l border-zinc-600 ml-[6px] mt-[10px]">
+          <ul className="flex flex-col gap-[5px] border-l border-zinc-600 ml-[6px] mt-[10px]">
             {verbGrouped.map((verb,index) => (
               <VerbCard
                 key={verb.id}
